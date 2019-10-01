4d72aec6d2810c7a7cfe10192e6af2becd3ca4e2
LiaLobo <lia.lobo@gmail.com>
2019-10-01 14:41:23 -0300

    feito botão de deletar, organização das classes dos botões no css e iniciamos o botão de dar check em todas as tarefas

--- js/script.js
+++ js/script.js
@@ -7,7 +7,6 @@
 // Criar um elemento p h1 
 // dar o conteudo value e posicionar  
 
-
 // criar um if else (caso a usuario insira uma string vazia)  
 
 const form = document.getElementById("form")
@@ -15,6 +14,11 @@ const form = document.getElementById("form")
 const inserirTarefa = document.getElementById("tarefasLista")
 
 const erro = document.querySelector('.error')
+
+const btnLimpar = document.querySelector('.btnLimpar')
+
+const btnFeito = document.querySelector('.btnFeito')
+
 form.addEventListener("submit", function(evento){
     evento.preventDefault()
     let mensagemUsuario = document.querySelector(".inputContainer")
@@ -26,34 +30,59 @@ form.addEventListener("submit", function(evento){
         let elementoLista = document.createElement("p")
         elementoLista.textContent = valorMensagem
         inserirTarefa.appendChild(elementoLista)
+        elementoLista.classList.add('borda')
+
+        // 2. criar uma const pra criar um novo elemento 
+        // // colocar conteudo x 
+        const excluir = document.createElement('span')
+        excluir.textContent = 'x'
+        elementoLista.appendChild(excluir)
+        excluir.classList.add('delete')
+        
         form.reset()
         
+        // 1. no css criar uma class pro javascript x
+        // criar o evento de tarefa 
+        // dentro desse evento colocar um if else, 
+        // se a pessoa clicar ele dá um check. 
+        // se clicar ele descheck. 
         elementoLista.addEventListener('click', function(evento){
             if (elementoLista.classList.contains('check')) {
                 elementoLista.classList.remove('check')
             } else {
                 elementoLista.classList.add('check')
             }
-
         })
+
+        // // evento click 
+        // // remover o x 
+        excluir.addEventListener('click', function() {
+            elementoLista.classList.add('remover')
+            excluir.classList.add('remover')
+        })
+
+        //criar evento no botão para dar check em todas as tarefas
+
+        btnFeito.addEventListener('click', function() {
+            inserirTarefa.classList.add('check')
+        
+            
+            
+        })
+
+        //criar evento no outro botão para limpar todas as tarefas
+
+
+
+
+
     }
-
-
-
-
 })
 
-// 1. no css criar uma class pro javascript x
-// criar o evento de tarefa 
-// dentro desse evento colocar um if else, 
-// se a pessoa clicar ele dá um check. 
-// se clicar ele descheck. 
 
 
-// 2. criar uma const pra criar um novo elemento 
-// // colocar conteudo x 
-// // evento click 
-// // remover o x 
+
+
 
 
 
